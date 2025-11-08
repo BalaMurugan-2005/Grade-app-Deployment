9722617de63aafad155ffda44450c3ca24951cdd
Bala Murugan P <pbalamurugan260@gmail.com>
2025-11-08 19:41:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // ✅ Allow requests from Live Server on port 5500
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500','https://grade-app-deployment.onrender.com'],
     credentials: true
 }));
 
