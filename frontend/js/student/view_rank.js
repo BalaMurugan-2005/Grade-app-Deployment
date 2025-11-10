8f7de5e16db3ad66c2eedfadf1eabd6e1d8964a2
Bala Murugan P <pbalamurugan260@gmail.com>
2025-11-10 13:42:40 +0530

    Update view_rank.js

--- frontend/js/student/view_rank.js
+++ frontend/js/student/view_rank.js
@@ -108,7 +108,7 @@ function initializeApp() {
     // Dashboard redirect
     if (dashboardBtn) {
         dashboardBtn.addEventListener('click', () => {
-            window.location.href = 'https://grade-app-deployment.onrender.com';
+            window.location.href = '../../templates/login.html';
         });
     }
 
